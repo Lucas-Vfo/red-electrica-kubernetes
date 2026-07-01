df50b303899fbca69164951b4c59499303ef071f
jorgeitonn <jorge.caceres.silva@alumnos.uta.cl>
2026-07-01 08:44:53 -0400

    feat(db): crear archivos de inicializacion de base de datos para los microservicios
    Scripts iniciales para separar las bases de datos de los 3 microservicios

---
+++ database/init-capacidad.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS transformadores (
+    id_transformador INT PRIMARY KEY,
+    capacidad_total_kw DECIMAL(8,2) NOT NULL,
+    capacidad_restante_kw DECIMAL(8,2) NOT NULL
+);
+
+INSERT INTO transformadores (id_transformador, capacidad_total_kw, capacidad_restante_kw)
+VALUES (1, 100.00, 100.00)
+ON CONFLICT (id_transformador) DO NOTHING;
